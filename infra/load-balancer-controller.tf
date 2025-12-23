aeffa3c2c720e99573c002b20396aa4e62305fe4
hucancode <hucancode@gmail.com>
2025-12-23 12:11:47 +0900

    add missing permission

--- infra/load-balancer-controller.tf
+++ infra/load-balancer-controller.tf
@@ -54,6 +54,7 @@ resource "aws_iam_policy" "load_balancer_controller" {
           "elasticloadbalancing:DescribeLoadBalancers",
           "elasticloadbalancing:DescribeLoadBalancerAttributes",
           "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeListenerAttributes",
           "elasticloadbalancing:DescribeListenerCertificates",
           "elasticloadbalancing:DescribeSSLPolicies",
           "elasticloadbalancing:DescribeRules",
